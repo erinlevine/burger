66812fd874610b3fab6cdab07a72e6903ad21fce
erinlevine <erinclevine@gmail.com>
2017-07-02 10:42:17 -0400

    Adding folder structure

---
+++ server.js
@@ -0,0 +1,9 @@
+// Require the following npm packages inside of the server.js file:
+
+// express
+// method-override
+// body-parser
+
+var express = require("express");
+var methodOverride = require("method-override");
+var bodyParser = require("body-parser");
