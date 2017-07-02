e2b024815896573d312f77c55a2b273065ac3a45
erinlevine <erinclevine@gmail.com>
2017-07-02 11:45:33 -0400

    Added ports

--- server.js
+++ server.js
@@ -7,3 +7,34 @@
 var express = require("express");
 var methodOverride = require("method-override");
 var bodyParser = require("body-parser");
+
+// Dependencies:
+
+var express = require("express");
+var bodyParser = require("body-parser");
+
+//Setting up express:
+var app = express();
+
+// Initial port:
+var PORT = process.env.PORT || 8080;
+
+// BodyParser:
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+
+// Routing:
+require("./app/routing/apiRoutes")(app);
+
+require('./app/routing/htmlRoutes')(app);
+
+
+// Listener:
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
+
+module.exports = server;
