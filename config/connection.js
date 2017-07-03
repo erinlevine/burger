cf6c92b8e522bd7f96ed1d4baa4a11afabd27727
erinlevine <erinclevine@gmail.com>
2017-07-02 23:28:43 -0400

    Added connections

--- config/connection.js
+++ config/connection.js
@@ -3,13 +3,21 @@
 // Export the connection.
 
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+
+else {
+  var connection = mysql.createConnection({
+    port: 3306,
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "burgers_db"
+  });
+}
 
 connection.connect(function(err) {
   if (err) {
@@ -19,6 +27,7 @@ connection.connect(function(err) {
   console.log("connected as id " + connection.threadId);
 });
 
+//exporting to orm.js
 module.exports = connection;
 
 
